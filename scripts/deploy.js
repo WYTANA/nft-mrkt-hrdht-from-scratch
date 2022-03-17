d54070fe565c985328c8d33195875deb2d221695
Travis L Orback <tlorback@gmail.com>
2022-03-16 19:22:44 -0500

    install OZ contracts, create new contract with OZ, create Luigi metadata, deploy Luigi

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,14 +2,15 @@ const {ethers} = require("hardhat")
 
 async function main() {
 
-  const SuperMarioWorld = await ethers.getContractFactory("SuperMarioWorld")
-  const superMarioWorld = await SuperMarioWorld.deploy("SuperMarioWorld", "SPRM")
+  const SuperMarioWorld = await ethers.getContractFactory("SuperMarioWorldOZ")
+  const superMarioWorld = await SuperMarioWorld.deploy("SuperMarioWorldOZ", "SPRMO")
 
   await superMarioWorld.deployed()
   console.log("Success! Contract deployed to: ", superMarioWorld.address)
 
+  // mint one at a time
   await superMarioWorld.mint(
-    "https://ipfs.io/ipfs/Qme3eLDoaujGqzacUeoydYtKQNaP67wjYLp5DAt3FRqqgB"
+    "https://ipfs.io/ipfs/QmTuqznk8ws9oy1Xer21BqoAYRRxgNBioEqK7LyDLJc2uW"
   )
   console.log("NFT successfully minted!")
 }
