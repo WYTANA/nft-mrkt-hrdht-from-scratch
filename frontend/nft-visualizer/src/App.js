72e3aa160309e8b3dd0de8fd0874b65ecdcd9038
Travis L Orback <tlorback@gmail.com>
2022-03-25 16:01:03 -0500

    add styling, map, and text to App component

--- frontend/nft-visualizer/src/App.js
+++ frontend/nft-visualizer/src/App.js
@@ -1,6 +1,31 @@
-import NFTCard from './components/NFTCard'
+import NFTCard from "./components/NFTCard"
+import styled from "styled-components"
 
 const nfts = [
+  {
+    name: "Mario",
+    symbol: "SMWC",
+    copies: 10,
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    name: "Luigi",
+    symbol: "SMWC",
+    copies: 10,
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    name: "Yoshi",
+    symbol: "SMWC",
+    copies: 10,
+    image: "https://via.placeholder.com/150",
+  },
+  {
+    name: "Donkey Kong",
+    symbol: "SMWC",
+    copies: 10,
+    image: "https://via.placeholder.com/150",
+  },
   {
     name: "Mario",
     symbol: "SMWC",
@@ -31,16 +56,41 @@ const App = () => {
   console.log(nfts)
   return (
     <div className="App">
-      
-      {
-        nfts.map((nft, i) => 
-          <NFTCard nft={nft} key={i} />
-        )
-      }
-    </div> 
+      <Container>
+        <Title>Super Mario World Collection</Title>
+        <Subtiltle>The rarest and best of Super Mario World!</Subtiltle>
+        <Grid>
+          {nfts.map((nft, i) => (
+            <NFTCard nft={nft} key={i} />
+          ))}
+        </Grid>
+      </Container>
+    </div>
   )
 }
 
+const Title = styled.h1`
+  margin: 0;
+  text-align: center;
+`
 
+const Subtiltle = styled.h4`
+  margin-top: 0;
+  text-align: center;
+  color: gray;
+`
+
+const Container = styled.div`
+  width: 70%;
+  max-width: 1200px;
+  margin: auto;
+  margin-top: 100px;
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  row-gap: 40px;
+`
 
 export default App
